db888e55e2ab2a96f9fda2368cdb22f257dab663
jorgeabrahan <jorge24abrahan@gmail.com>
2023-04-10 16:19:24 -0600

    Create student class constructor

---
+++ student.rb
@@ -0,0 +1,8 @@
+require './person'
+
+class Student < Person
+  def initialize(classroom, age, name = "Unknown", parent_permission = true)
+    super(age, name, parent_permission)
+    @classroom = classroom
+  end
+end
