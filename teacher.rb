da38bff2630775bd30e3a9ceab341da140a7fa62
jorgeabrahan <jorge24abrahan@gmail.com>
2023-04-10 16:20:11 -0600

    Create teacher class constructor

---
+++ teacher.rb
@@ -0,0 +1,8 @@
+require './person'
+
+class Teacher < Person
+  def initialize(specialization, age, name = "Unknown", parent_permission = true)
+    super(age, name, parent_permission)
+    @specialization = specialization
+  end
+end
