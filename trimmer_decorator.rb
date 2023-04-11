e2932b1e585d11abebdf016cb9ae4b19ec3ed496
jorgeabrahan <jorge24abrahan@gmail.com>
2023-04-11 13:44:36 -0600

    Replace super for call from nameable instance variable

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -2,7 +2,7 @@ require './decorator'
 
 class TrimmerDecorator < Decorator
   def correct_name
-    output_correct_name = super()
+    output_correct_name = @nameable.correct_name()
     return output_correct_name[0, 11] if output_correct_name.length > 10
     output_correct_name
   end
