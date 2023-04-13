316c3d0cea8092216585c092b7c5c24616ac4637
jorgeabrahan <jorge24abrahan@gmail.com>
2023-04-13 08:19:17 -0600

    Create main function

--- main.rb
+++ main.rb
@@ -1,10 +1,6 @@
-require './person'
-require './capitalize_decorator'
-require './trimmer_decorator'
+require './app'
 
-person = Person.new(22, 'maximilianus')
-print("#{person.correct_name}\n")
-capitalized_person = CapitalizeDecorator.new(person)
-print("#{capitalized_person.correct_name}\n")
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-print("#{capitalized_trimmed_person.correct_name}\n")
+def main
+end
+
+main()
