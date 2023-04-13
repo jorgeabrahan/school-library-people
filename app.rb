5d715fcba97b496c26fa219b598116b5ed0cda9d
jorgeabrahan <jorge24abrahan@gmail.com>
2023-04-13 08:15:09 -0600

    Add method to create a person

--- app.rb
+++ app.rb
@@ -4,5 +4,12 @@ require './book'
 require './rental'
 
 class App
-  
+  attr_accessor :persons, :books, :rentals
+
+  def create_person(age, name = "Unknown", parent_permission = true, specialization = nil)
+    persons.push(
+      specialization != nil ?
+      Teacher.new(specialization, age, name) : Student.new(age, name, parent_permission)
+    )
+  end
 end
