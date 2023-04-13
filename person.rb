d742b3659be6047b218291fc2d0b9d84a850ba2f
jorgeabrahan <jorge24abrahan@gmail.com>
2023-04-13 12:27:39 -0600

    Generate random id when creating a person

--- person.rb
+++ person.rb
@@ -5,6 +5,7 @@ class Person < Nameable
   attr_accessor :name, :age, :rentals
   def initialize(age, name = "Unknown", parent_permission = true)
     super()
+    @id = SecureRandom.random_number(10000)
     @name = name
     @age = age
     @parent_permission = parent_permission
