7e9eed053497e02e8f4881329792167f0f1e14b7
jorgeabrahan <jorge24abrahan@gmail.com>
2023-04-13 10:27:13 -0600

    Get person data and create it

--- main.rb
+++ main.rb
@@ -13,40 +13,101 @@ def options_process
   option = gets.chomp.to_i
 end
 
+def options_create_person
+  puts "Which type of person do you want to create?"
+  puts "1- Student"
+  puts "2- Teacher"
+  print "Write the number corresponding to the option you want to choose: "
+  option = gets.chomp.to_i
+end
+
+# determine if an option is valid (is in range)
 def is_option_valid?(option, max, min = 1)
   return (option > max || option < min)
 end
 
+# calls a function that gets an option while the option is not valid (not in range)
 def get_valid_option(get_option_func, max)
   option = 0
   while is_option_valid?(option, max)
+    # keep asking for option while is not valid
     option = get_option_func()
-    if not(is_option_valid?(option, max)) puts "\nOption is not valid!\n"
+    # if it's not valid (it's not in valid range), display a message
+    puts "\nOption is not valid!\n" if not(is_option_valid?(option, max))
   end
   option
 end
 
+def get_boolean_answer(question)
+  answer = ""
+  valid_answers = ["y", "Y", "n", "N", "Yes", "No", "YES", "NO"]
+  # while answer is not valid
+  while not(valid_answer.include?(answer))
+    # keep asking and getting answer
+    print question
+    answer = gets.chomp
+    # if it's not valid (answer is not a valid answer from array, display a message)
+    puts "\nAnswer is not valid!\n" if not(valid_answer.include?(answer))
+  end
+  # return answer as boolean (if it starts with "y" is true otherwise it is false)
+  return (answer.downcase[0] == "y")
+end
+
+def get_not_empty_answer(question)
+  answer = ''
+  while answer == ''
+    print question
+    answer = gets.chomp
+  end
+  answer
+end
+
+def get_person_data()
+  # types: (1) student, (2) teacher
+  type_person = get_valid_option(options_create_person, 2)
+  # get common fields between persons
+  print "Age: "
+  age = gets.chomp
+
+  print "Name (leave empty for unknown): "
+  name = gets.chomp
+  name = "Unknown" if (name == nil || name == '')
+
+  # by default permission is true for teachers
+  permission = true
+  permission = get_boolean_answer("Has parent permission? [Y/N]: ") if (type_person == 1)
+
+  # by default specialization is nil for student
+  specialization = nil
+  specialization = get_not_empty_answer("Specialization: ") if (type_person == 2)
+
+  return [age, name, permission, specialization]
+end
+
 def main
   option = 0
+  app = App.new
 
   until option == 7
     option = get_valid_option(options_process, 7)
-    
+
     case option
     when 1
-      # create a person
+      # CREATE A PERSON
+      age, name, permission, specialization = get_person_data()
+      app.create_person(age, name, permission, specialization)
     when 2
-      # create a book
+      # CREATE A BOOK
     when 3
-      # create a rental
+      # CREATE A RENTAL
     when 4
-      # list all persons
+      # LIST ALL PERSONS
     when 5
-      # list all books
+      # LIST ALL BOOKS
     when 6
-      # list all rentals for a given person id
+      # LIST ALL RENTALS FOR A GIVEN PERSON ID
     else
-      # exit app
+      # EXIT APP
       puts "\nThanks for using the app!\n"
     end
   end
