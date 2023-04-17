f36881664149d50d2824395cf4347e65f5d4be6c
jorgeabrahan <jorge24abrahan@gmail.com>
2023-04-17 14:00:13 -0600

    Create comments to explain each method in the app class

--- app.rb
+++ app.rb
@@ -15,9 +15,13 @@ class App
   def list_persons(display_num: false)
     puts "Amount of persons #{persons.length}"
     persons.each_with_index do |p, index|
+      # identify which type to display
       type = p&.specialization.nil? ? '[Student]' : '[Teacher]'
+      # identify if it is needed to display the specialization
       specialization = type == '[Teacher]' ? ", Specialization: #{p.specialization}" : ''
+      # display the index of the person if necessary
       num = display_num ? "#{index}) " : ''
+      # display each person data
       puts "#{num}#{type} ID: #{p.id}, Name: #{p.name}, Age: #{p.age}#{specialization}"
     end
   end
@@ -25,22 +29,41 @@ class App
   def list_books(display_num: false)
     puts "Amount of books #{books.length}"
     books.each_with_index do |b, index|
+      # display each book data and the index if necessary
       puts "#{display_num ? "#{index}) " : ''}Title: \"#{b.title}\", Author: #{b.author}"
     end
   end
 
+  def get_person(id)
+    # get person from persons array by ID
+    person = persons.find { |p| p.id == id }
+    # if person does not exist returns nil and displays an error message
+    if person.nil? || person&.rentals.nil?
+      puts 'id is not valid!'
+      return nil
+    end
+    # otherwise the person is returned
+    person
+  end
+
   def list_rentals(id)
+    # get person object from id
     person = get_person(id)
+    # if person does not exist then do nothing
     return if person.nil?
 
+    # display the amount of rentals
     puts "#{person.name} rentals (#{person&.rentals&.length}):"
     person&.rentals&.each do |r|
+      # display rentals information for the entered person
       puts "Date: #{r.date}, Book title: \"#{r.book.title}\", Book author: #{r.book.author}"
     end
   end
 
   def create_person(age, name = 'Unknown', specialty = nil, parent_permission: true)
+    # determine which type of person to insert
     p = specialty.nil? ? Student.new(age, name, parent_permission) : Teacher.new(specialty, age, name)
+    # insert the person to the persons array
     persons.push(p)
   end
 
@@ -50,14 +73,5 @@ class App
 
   def create_rental(date, book, person)
     rentals.push(Rental.new(date, book, person))
-  end
-
-  def get_person(id)
-    person = persons.find { |p| p.id == id }
-    if person.nil? || person&.rentals.nil?
-      puts 'id is not valid!'
-      return nil
-    end
-    person
   end
 end
