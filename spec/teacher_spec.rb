3e419080a955af9bc410f551ee1e03bd738e2c07
jorgeabrahan <jorge24abrahan@gmail.com>
2023-04-19 17:19:38 -0600

    Create tests for the new method whenever a teacher is initialized

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -4,4 +4,27 @@ describe Teacher do
   before :all do
     @teacher = Teacher.new "Software Dev", 35, "Juan"
   end
+
+  describe "#new" do
+    it "Should create an instance of the Teacher class" do
+      expect(@teacher).to be_an_instance_of Teacher
+    end
+
+    it "Should initialize specialization as 'Software Dev'" do
+      expect(@teacher.specialization).to eq "Software Dev"
+    end
+
+    it "Should initialize age as 35" do
+      expect(@teacher.age).to be 35
+    end
+
+    it "Should initialize name as Juan" do
+      expect(@teacher.name).to eq "Juan"
+    end
+
+    it "Should generate a random number as id between 100 and 999 for the teacher" do
+      expect(@teacher.id).to be_kind_of Numeric
+      expect(@teacher.id).to be_between(100, 999).inclusive
+    end
+  end
 end
