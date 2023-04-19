8cf547baccc2941cbb8886413802575fc70b468c
jorgeabrahan <jorge24abrahan@gmail.com>
2023-04-18 18:38:57 -0600

    Create a preserve method to store data in the respective file as json

--- storage.rb
+++ storage.rb
@@ -4,4 +4,8 @@ class Storage
   def initialize(relative_path)
     @relative_path = relative_path
   end
+
+  def preserve(data, file_name)
+    File.write("./#{@relative_path}/#{file_name}", data.to_json)
+  end
 end
