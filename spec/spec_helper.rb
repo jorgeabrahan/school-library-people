cb4210e24b0b0cfc841b2543d95cb9e2782fba97
jorgeabrahan <jorge24abrahan@gmail.com>
2023-04-19 14:44:57 -0600

    Create spec helper file to import all classes in order to test them

---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+# require classes to test
+relative_require '../person'
+relative_require '../book'
+relative_require '../teacher'
+relative_require '../student'
+relative_require '../classroom'
+relative_require '../rental'
