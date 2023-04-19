0fe8bff534d4f2d8a567c024c99872206a345d6b
jorgeabrahan <jorge24abrahan@gmail.com>
2023-04-18 18:39:40 -0600

    Create a read method to get data and parse it from file

--- storage.rb
+++ storage.rb
@@ -8,4 +8,9 @@ class Storage
   def preserve(data, file_name)
     File.write("./#{@relative_path}/#{file_name}", data.to_json)
   end
+
+  def read(file_name)
+    file = File.read("./#{@relative_path}/#{file_name}")
+    data = JSON.parse(file)
+  end
 end
