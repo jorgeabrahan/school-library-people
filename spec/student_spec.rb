bbd766bb64316177596a7ea1de72b9577c4ad895
jorgeabrahan <jorge24abrahan@gmail.com>
2023-04-19 17:26:29 -0600

    Fix rubocop linter errors

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -2,33 +2,33 @@ require 'spec_helper'
 
 describe Student do
   before :all do
-    @student = Student.new 20, "Jorge"
+    @student = Student.new 20, 'Jorge'
   end
 
-  describe "#new" do
-    it "Should create an instance of the Student class" do
+  describe '#new' do
+    it 'Should create an instance of the Student class' do
       expect(@student).to be_an_instance_of Student
     end
 
-    it "Should initialize age as 20" do
+    it 'Should initialize age as 20' do
       expect(@student.age).to be 20
     end
 
-    it "Should initialize name as Jorge" do
-      expect(@student.name).to eq "Jorge"
+    it 'Should initialize name as Jorge' do
+      expect(@student.name).to eq 'Jorge'
     end
 
-    it "Should generate a random number as id between 100 and 999 for the student" do
+    it 'Should generate a random number as id between 100 and 999 for the student' do
       expect(@student.id).to be_kind_of Numeric
       expect(@student.id).to be_between(100, 999).inclusive
     end
   end
 
-  describe "#as_json" do
-    it "Should convert the student data to JSON" do
+  describe '#as_json' do
+    it 'Should convert the student data to JSON' do
       mock_student = {
         id: @student.id,
-        name: "Jorge",
+        name: 'Jorge',
         age: 20,
         parent_permission: true,
         specialization: nil
@@ -37,21 +37,21 @@ describe Student do
     end
   end
 
-  describe "#classroom_setter" do
-    it "Should add the student to a classroom" do
-      @student.classroom = Classroom.new "Programming"
-      expect(@student.classroom.label).to eq "Programming"
+  describe '#classroom_setter' do
+    it 'Should add the student to a classroom' do
+      @student.classroom = Classroom.new 'Programming'
+      expect(@student.classroom.label).to eq 'Programming'
     end
   end
 
-  describe "#play_hooky" do
-    it "Should return a string" do
-      expect(@student.play_hooky).to eq "¯\\(ツ)/¯"
+  describe '#play_hooky' do
+    it 'Should return a string' do
+      expect(@student.play_hooky).to eq '¯\\(ツ)/¯'
     end
   end
 
-  describe "#method_missing" do
-    it "Should return nil whenever you call a non existing method of the student class" do
+  describe '#method_missing' do
+    it 'Should return nil whenever you call a non existing method of the student class' do
       expect(@student.non_existing_method).to be_nil
     end
   end
--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -2,37 +2,37 @@ require 'spec_helper'
 
 describe Teacher do
   before :all do
-    @teacher = Teacher.new "Software Dev", 35, "Juan"
+    @teacher = Teacher.new 'Software Dev', 35, 'Juan'
   end
 
-  describe "#new" do
-    it "Should create an instance of the Teacher class" do
+  describe '#new' do
+    it 'Should create an instance of the Teacher class' do
       expect(@teacher).to be_an_instance_of Teacher
     end
 
     it "Should initialize specialization as 'Software Dev'" do
-      expect(@teacher.specialization).to eq "Software Dev"
+      expect(@teacher.specialization).to eq 'Software Dev'
     end
 
-    it "Should initialize age as 35" do
+    it 'Should initialize age as 35' do
       expect(@teacher.age).to be 35
     end
 
-    it "Should initialize name as Juan" do
-      expect(@teacher.name).to eq "Juan"
+    it 'Should initialize name as Juan' do
+      expect(@teacher.name).to eq 'Juan'
     end
 
-    it "Should generate a random number as id between 100 and 999 for the teacher" do
+    it 'Should generate a random number as id between 100 and 999 for the teacher' do
       expect(@teacher.id).to be_kind_of Numeric
       expect(@teacher.id).to be_between(100, 999).inclusive
     end
   end
 
-  describe "#as_json" do
-    it "Should convert the teacher data to JSON" do
+  describe '#as_json' do
+    it 'Should convert the teacher data to JSON' do
       mock_teacher = {
         id: @teacher.id,
-        name: "Juan",
+        name: 'Juan',
         age: 35,
         parent_permission: true,
         specialization: @teacher.specialization
@@ -41,9 +41,9 @@ describe Teacher do
     end
   end
 
-  describe "#can_use_services?" do
-    it "Should always be able to use services" do
-      expect(@teacher.can_use_services?).to eq "Programming"
+  describe '#can_use_services?' do
+    it 'Should always be able to use services' do
+      expect(@teacher.can_use_services?).to eq 'Programming'
     end
   end
 end
