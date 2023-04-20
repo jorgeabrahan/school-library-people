aee7a08357e108050101e72b208988faa8902af2
jorgeabrahan <jorge24abrahan@gmail.com>
2023-04-20 13:58:51 -0600

    Create solver spec file to test the class

---
+++ spec/solver_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe Solver do
+  describe '#factorial' do
+    it '' do
+      
+    end
+  end
+
+  describe '#reverse' do
+    it '' do
+      
+    end
+  end
+  
+  describe '#fizzbuzz' do
+    it '' do
+      
+    end
+  end
+end
