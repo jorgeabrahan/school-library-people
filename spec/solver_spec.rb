a6a39000e5a5aa51fb41b4d76e1516d7f95a6315
Ayomipo Majek <ayomipomajek@gmail.com>
2023-04-20 21:14:33 +0100

    reverse test created

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -8,8 +8,11 @@ describe Solver do
   end
 
   describe '#reverse' do
-    it '' do
-      
+    it 'returns the reversed word' do
+      solver = Solver.new
+      expect(solver.reverse('')).to eq('')
+      expect(solver.reverse('hello')).to eq('olleh')
+      expect(solver.reverse('racecar')).to eq('racecar')
     end
   end
   
