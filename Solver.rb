834a18236b84491d3e5d3cc6a6450a7e0e13554b
NelsonAraujo93 <nelsonaraujoparedes93@hotmail.com>
2023-04-20 15:31:42 -0500

    Add fizzbuzz function based on tdd

--- Solver.rb
+++ Solver.rb
@@ -10,6 +10,14 @@ class Solver
   end
 
   def fizzbuzz(n)
-
+    if n%3 == 0 && n%5 == 0
+      return 'fizzbuzz'
+    elsif n%3 == 0
+      return 'fizz'
+    elsif n%5 == 0
+      return 'buzz'
+    else
+      return n.to_s
+    end
   end
 end
