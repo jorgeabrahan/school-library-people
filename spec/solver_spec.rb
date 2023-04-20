08d1bf3eefe31adb7b451c793f385334cf74df0a
NelsonAraujo93 <nelsonaraujoparedes93@hotmail.com>
2023-04-20 15:23:25 -0500

    Modify and add on last test to fizzbuzz function

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -47,13 +47,16 @@ describe Solver do
   describe '#fizzbuzz' do
     context 'takes an integer and return fizz, buzz or fizzbuzz depending if' do
       it 'it divisible by 3 return fizz, gets 6 expected fizz' do
-        expect(@solver.fizzbuzz(3)).to eq('fizz')
+        expect(@solver.fizzbuzz(6)).to eq('fizz')
       end
       it 'it divisible by 5 return buzz, gets 10 expected fizz' do
-        expect(@solver.fizzbuzz(5)).to eq('buzz')
+        expect(@solver.fizzbuzz(10)).to eq('buzz')
       end
       it 'it divisible by 3 and 5 return fizzbuzz, gets 15 expected fizz' do
         expect(@solver.fizzbuzz(15)).to eq('fizzbuzz')
+      end
+      it 'its not divisible by 3 or 5 return N as a String, gets 8 expected "8"' do
+        expect(@solver.fizzbuzz(8)).to eq('8')
       end
     end
   end
